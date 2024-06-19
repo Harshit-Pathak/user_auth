97b8ad11167c815fda68141f5e99d9aa2cdf193a
Harshit-Pathak <49994175+Harshit-Pathak@users.noreply.github.com>
2024-06-19 21:19:22 +0530

    Update auth_handler.py

--- auth/auth_handler.py
+++ auth/auth_handler.py
@@ -4,22 +4,7 @@ from .utils import decode_token
 
 
 class JWTBearer(HTTPBearer):
-    def __init__(self, auto_error: bool = True):
-        super(JWTBearer, self).__init__(auto_error=auto_error)
-
-    async def __call__(self, request: Request) -> HTTPAuthorizationCredentials:
-        credentials: HTTPAuthorizationCredentials = await super(
-            JWTBearer, self
-        ).__call__(request)
-        if credentials:
-            if not self.verify_jwt(credentials.credentials):
-                raise HTTPException(
-                    status_code=403, detail="Invalid token or expired token."
-                )
-            return credentials
-        else:
-            raise HTTPException(status_code=403, detail="Invalid authorization code.")
-
+    
     def verify_jwt(self, token: str) -> bool:
         try:
             data = decode_token(token)
